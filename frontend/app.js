41dc7bd9a1cf46ed94027720929502066620bab3
Levi PC <90425048+Majestic7code@users.noreply.github.com>
2026-02-17 20:13:03 -0300

    progress bar added

---
+++ frontend/app.js
@@ -0,0 +1,67 @@
+const fomeValor = document.querySelector('#fome-valor')
+const energiaValor = document.querySelector('#energia-valor')
+const humorValor = document.querySelector('#humor-valor')
+const btnAlimentar = document.querySelector('#btn-alimentar')
+const btnDormir = document.querySelector('#btn-dormir')
+const btnBrincar = document.querySelector('#btn-brincar')
+const fomeBarra = document.querySelector('#fome-barra')
+const energiaBarra = document.querySelector('#energia-barra')
+const humorBarra = document.querySelector('#humor-barra')
+//Objeto - PET
+const pet = { 
+    fome: 100,
+    energia: 100,
+    humor: 100,
+}
+//Funções de ações
+function atualizarStatus() {
+    fomeValor.textContent = pet.fome
+    energiaValor.textContent = pet.energia
+    humorValor.textContent = pet.humor
+
+    fomeBarra.value = pet.fome
+    energiaBarra.value = pet.energia
+    humorBarra.value = pet.humor
+}
+atualizarStatus()
+
+function alimentar() {
+    pet.fome = Math.min(100, pet.fome + 10)
+    atualizarStatus()
+}
+
+function dormir() {
+    pet.energia = Math.min(100, pet.energia + 10)
+    atualizarStatus()
+}
+
+function brincar() {
+    pet.humor = Math.min(100, pet.humor + 10)
+    atualizarStatus()
+}
+
+btnAlimentar.addEventListener('click', alimentar)
+btnDormir.addEventListener('click',dormir)
+btnBrincar.addEventListener('click', brincar)
+
+function decairStatus() {
+    pet.fome = Math.max(0, pet.fome - 5)
+    pet.energia = Math.max(0, pet.energia - 8)
+    pet.humor = Math.max(0, pet.humor - 7)
+
+    if (pet.fome < 20) {
+        console.log("⚠️ PET COM FOME! ")
+    }
+
+    if (pet.energia < 25) {
+        console.log("😴 PET COM SONO!")
+    }
+
+    if (pet.humor < 22) {
+        console.log("🫩 PET ENTENDIADO!")
+    }
+
+    atualizarStatus()
+}
+
+setInterval(decairStatus, 2000)
